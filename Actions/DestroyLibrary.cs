e9557c1259becff882b82bcc3dfa6dab4fffeda6
InvertGames <invertgamestudios@gmail.com>
2015-09-13 18:04:02 -0400

    no message

--- Actions/DestroyLibrary.cs
+++ Actions/DestroyLibrary.cs
@@ -14,11 +14,15 @@ namespace uFrame.Actions
             UnityEngine.Object.Destroy(behaviour);
         }
         [ActionTitle("Destroy Entity")]
-        public static void DestroyEntity(int entityId)
+        public static void DestroyEntity(int entityId, float time)
         {
-            UnityEngine.Object.Destroy(EntityService.GetEntityView(entityId).gameObject);
+            UnityEngine.Object.Destroy(EntityService.GetEntityView(entityId).gameObject, time);
         }
-
+        [ActionTitle("Destroy GameObject")]
+        public static void DestroyGameObject(GameObject gameObject, float time)
+        {
+            UnityEngine.Object.Destroy(gameObject, time);
+        }
         [ActionTitle("Destroy Timer")]
         public static void DestroyTimer(IDisposable timer)
         {
