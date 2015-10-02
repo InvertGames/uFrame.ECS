67bd51c4df8412bbbec0c217303475a33f14d1aa
nitreo <sinitreo@gmail.com>
2015-10-02 10:03:27 +0200

    Milliseconds timer added

--- Actions/Timer.cs
+++ Actions/Timer.cs
@@ -19,5 +19,20 @@ namespace uFrame.Actions
                 Complete();
             }).DisposeWith(System);
         }
+    }  
+    
+    [ActionTitle("Timer (Milliseconds)"),uFrameCategory("Timers")]
+    public class MillitTimer : UFAction
+    {
+        [In] public int Milliseconds;
+
+        [Out] public Action Complete;
+        public override void Execute()
+        {
+            Observable.Timer(new TimeSpan(0, 0, 0, 0, Milliseconds)).Subscribe(_ =>
+            {
+                Complete();
+            }).DisposeWith(System);
+        }
     }
 }
